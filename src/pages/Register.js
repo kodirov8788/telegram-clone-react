2358d0d97abda91d67b6e248e7d0fbc12883fd32
kodirov8788 <kodirov8788@gmail.com>
2022-10-12 10:01:52 +0500

    asdsa

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,22 +7,62 @@ import PhoneInput from 'react-phone-number-input'
 import TelegramImg from "../images/telegram.svg"
 function Register() {
     const [config, setConfig] = useState({
-        number: "",
+        number: "+998",
         varifyCode: "",
         flag: false,
-        comfirmObj: "",
+        confirmObj: "",
         error: ""
     })
+    console.log(config)
     const stl = {
-        register: "",
-        img: "w-1/5"
+        register: "flex flex-col items-center",
+        img: "w-1/5",
+        h1: "text-[40px] my-3 font-semibold",
+        p: "my-2 w-[300px] text-center text-gray-500",
+        input: "input text-[30px] p-2 flex flex-col items-center  ",
+        btn: "text-[30px] border border-black p-2 bg-red-500 rounded hover:bg-blue-400 duration-300"
     }
 
-    return (
-        <div className={stl.register}>
-            <img className={stl.img} src={TelegramImg} alt="" />
+    function setRecaptcha(PhoneNumber) {
+        console.log(PhoneNumber)
+        const Recaptchavarifier = new RecaptchaVerifier("recaptcha-container", {}, auth)
+        Recaptchavarifier.render()
+        return signInWithPhoneNumber(auth, PhoneNumber, Recaptchavarifier)
+    }
 
-        </div>
+    const HundleFunc = async (e) => {
+        e.preventDefault()
+        if (config.number === undefined || config.number === "" || config.number === "+998") {
+            setConfig({ ...config, error: "Nimadur xato" })
+
+        } else {
+            try {
+                console.log("sdadas")
+                const response = await setRecaptcha(config.number)
+                console.log(response)
+                setConfig({ ...config, flag: true, confirmObj: response })
+            }
+            catch (error) {
+                setConfig({ ...config, error: "catch Nimadur xato" })
+            }
+        }
+
+    }
+    return (
+        <form className={stl.register} onSubmit={HundleFunc}>
+            <img className={stl.img} src={TelegramImg} alt="" />
+            <h1 className={stl.h1}>Sign in to Telegram</h1>
+            <p className={stl.p}>Please confirm your country and
+                enter your phone number.</p>
+            <PhoneInput
+                className={stl.input}
+                defaultCountry='UZ'
+                value={config.number}
+                onChange={(e) => setConfig({ ...config, number: e })}
+            />
+            <button className={stl.btn}>Next</button>
+            <div id="recaptcha-container"></div>
+        </form>
     )
 }
 
